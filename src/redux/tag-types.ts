43f83211e2894421dc7b5fca80c15836be422940
md milon hosen <it000025@gms.com>
2023-10-13 23:00:13 +0600

    initial setup next app

---
+++ src/redux/tag-types.ts
@@ -0,0 +1,13 @@
+export enum tagTypes {
+  user = 'user',
+  service = 'service',
+  blog = 'blog',
+  faq = 'faq',
+}
+
+export const tagTypesList = [
+  tagTypes.user,
+  tagTypes.service,
+  tagTypes.blog,
+  tagTypes.faq,
+];
